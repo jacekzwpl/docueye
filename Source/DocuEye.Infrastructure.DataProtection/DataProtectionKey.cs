d03066522616a469cba5873faabce20b380b5c21
Jacek Zwoliński <jacek.zwolinski@htsoft.pl>
2024-01-27 21:51:01 +0100

    data protection infra start

---
+++ Source/DocuEye.Infrastructure.DataProtection/DataProtectionKey.cs
@@ -0,0 +1,12 @@
+﻿using DocuEye.Infrastructure.Persistence.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DocuEye.Infrastructure.DataProtection
+{
+    public class DataProtectionKey : BaseEntity
+    {
+        public string Key { get; set; } = null!;
+    }
+}
---
+++ Source/DocuEye.Infrastructure.DataProtection/MongoDbXmlRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.DataProtection.Repositories;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace DocuEye.Infrastructure.DataProtection
+{
+    public class MongoDbXmlRepository : IXmlRepository
+    {
+        private readonly IDataProtectionDBContext dbContext; 
+
+        public MongoDbXmlRepository(IDataProtectionDBContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public IReadOnlyCollection<XElement> GetAllElements()
+        {
+            var keys = this.dbContext.DataProtectionKeys.Find(o => true).GetAwaiter().GetResult();
+            return keys.Select(element => XElement.Parse(element.Key)).ToList().AsReadOnly();
+           
+        }
+
+        public void StoreElement(XElement element, string friendlyName)
+        {
+            var keyElement = new DataProtectionKey
+            {
+                Id = Guid.NewGuid().ToString(),
+                Key = element.ToString(SaveOptions.DisableFormatting)
+            };
+            this.dbContext.DataProtectionKeys.InsertOneAsync(keyElement).GetAwaiter().GetResult();
+        }
+    }
+}
