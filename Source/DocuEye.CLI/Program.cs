2edf846582c942f648bc1c80f0c2276fc7709293
Jacek Zwoliński <jacek.zwolinski@htsoft.pl>
2024-01-30 22:51:27 +0100

    start working on cli

--- Source/DocuEye.CLI/Program.cs
+++ Source/DocuEye.CLI/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+
+var logger = LoggerFactory.Create(config =>
+{
+    config.AddConsole();
+}).CreateLogger("StartupLogger");
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+builder.Configuration.AddCommandLine(args);
+
+//
+
+using IHost host = builder.Build();
+bool isImport = false;
+Boolean.TryParse(builder.Configuration["import"], out isImport);
+
+logger.LogInformation(builder.Configuration["adminToken"]);
+
+
+
+//await host.RunAsync();
+
+
+
+
