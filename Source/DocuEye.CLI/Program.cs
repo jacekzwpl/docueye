c31c1ae85a2cc73dc10f6a87b8da539db97be1b1
Jacek Zwoliński <jacek.zwolinski@htsoft.pl>
2024-02-02 17:32:35 +0100

    working cli

--- Source/DocuEye.CLI/Program.cs
+++ Source/DocuEye.CLI/Program.cs
@@ -2,7 +2,6 @@
 using DocuEye.CLI;
 using DocuEye.CLI.ApiClient;
 using DocuEye.CLI.Application.Services.ImportWorkspace;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -14,47 +13,44 @@ var logger = LoggerFactory.Create(config =>
     config.AddConsole();
 }).CreateLogger("StartupLogger");
 
-
-//builder.Configuration.AddCommandLine(args);
-
-Parser.Default.ParseArguments<CommandLineOptions>(args).WithParsed<CommandLineOptions>(options =>
+await Parser.Default.ParseArguments<CommandLineOptions>(args).MapResult(async (options) =>
 {
     HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
     builder.Services.AddHttpClient<IDocuEyeApiClient, DocuEyeApiClient>(
-    o => {
-        o.BaseAddress = new Uri(options.DocueyeAddress);
-        o.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", options.AdminToken);
-    }
-);
+        o =>
+        {
+            o.BaseAddress = new Uri(options.DocueyeAddress);
+            o.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", options.AdminToken);
+        }
+    );
 
     builder.Services.AddLogging(builder => builder.AddConsole());
 
     builder.Services.AddTransient<IImportWorkspaceService, ImportWorkspaceService>();
     using IHost host = builder.Build();
 
-});
+    if (options.Import == "workspace")
+    {
+        var importService = host.Services.GetRequiredService<IImportWorkspaceService>();
+        var parameters = new ImportWorkspaceParameters(
+            options.ImportKey,
+            options.WorkspaceFile,
+            options.WorkspaceId,
+            options.SourceLink);
+        var result = await importService.Import(parameters);
+        if(!result)
+        {
+            Environment.ExitCode = -1;
+        }
+    }
+    else
+    {
+        Environment.ExitCode = -1;
+        logger.LogError("--import value {0} is not valid", options.Import);
+    }
 
+}, errors => Task.FromResult(-1));
 
-
-
-
-//bool isImport = false;
-//Boolean.TryParse(builder.Configuration["import"], out isImport);
-
-//logger.LogInformation(builder.Configuration["adminToken"]);
-
-
-
-//var importService = host.Services.GetRequiredService<IImportWorkspaceService>();
-/*
-var parameters = new ImportWorkspaceParameters(
-    Guid.NewGuid().ToString(), 
-    "", 
-    "");
-await importService.Import(parameters);*/
-
-
-//await host.RunAsync();
 
 
 
