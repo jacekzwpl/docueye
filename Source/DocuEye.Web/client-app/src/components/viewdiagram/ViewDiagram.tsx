40c8f8c3707a55bed64825f8217cdacfb42cf1f1
Jacek Zwoliński <jacek.zwolinski@htsoft.pl>
2024-02-04 12:29:12 +0100

    fix type

--- Source/DocuEye.Web/client-app/src/components/viewdiagram/ViewDiagram.tsx
+++ Source/DocuEye.Web/client-app/src/components/viewdiagram/ViewDiagram.tsx
@@ -7,7 +7,7 @@ import 'reactflow/dist/style.css';
 import './edges/floatingedges.css'
 import { edgeTypes } from "./edges";
 import { AxiosResponse } from "axios";
-import { ComponentView, ContainerView, DeploymentView, DynamicView, Element, ImageView, SystemContextView, SystemLandscapeView, ViewConfiguration } from "../../api/docueye-api";
+import { ComponentView, ContainerView, DeploymentView, DynamicView, Element, FilteredView, ImageView, SystemContextView, SystemLandscapeView, ViewConfiguration } from "../../api/docueye-api";
 import Loader from "../loader";
 import { prepareDiagramElements } from "./functions/prepareDiagramElements";
 import { prepareDynamicDiagramElements } from "./functions/prepareDynamicDiagramElements";
@@ -115,7 +115,7 @@ const ViewDiagram = (props: IViewDiagramProps) => {
         setIsLoading(true);
         DocuEyeApi.ViewsApi
             .apiWorkspacesWorkspaceIdViewsFilteredIdGet(workspaceId, viewId)
-            .then((response: AxiosResponse<ComponentView>) => {
+            .then((response: AxiosResponse<FilteredView>) => {
                 if (response.data.elements && response.data.relationships) {
                     const {layoutedNodes, layoutedEdges} = prepareDiagramElements(response.data.elements, response.data.relationships, viewConfiguration, "ComponentView");
                     setNodes(layoutedNodes);
