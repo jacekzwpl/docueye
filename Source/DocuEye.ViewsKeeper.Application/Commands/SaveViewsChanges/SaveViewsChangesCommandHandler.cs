1db9662df67a97d3cf34f92f0942f05ef98fd4f4
Jacek Zwoliński <jacek.zwolinski@htsoft.pl>
2024-02-21 19:04:49 +0100

    Save views change

--- Source/DocuEye.ViewsKeeper.Application/Commands/SaveViewsChanges/SaveViewsChangesCommandHandler.cs
+++ Source/DocuEye.ViewsKeeper.Application/Commands/SaveViewsChanges/SaveViewsChangesCommandHandler.cs
@@ -37,15 +37,15 @@ namespace DocuEye.ViewsKeeper.Application.Commands.SaveViewsChanges
                 var keys = request.SystemLandscapeViews.Select(o => o.Key).ToArray();
                 await this.DeleteMissing(request.WorkspaceId, keys, ViewType.SystemLandscapeView);
                 // Get existing views 
-                var existingViews = await this.GetExisting(request.WorkspaceId, ViewType.SystemLandscapeView);
+                //var existingViews = await this.GetExisting(request.WorkspaceId, ViewType.SystemLandscapeView);
                 // Save views
                 foreach(var view in request.SystemLandscapeViews)
                 {
-                    var existingView = existingViews.SingleOrDefault(o => o.Key == view.Key);
-                    if(existingView != null)
-                    {
-                        view.Id = existingView.Id;
-                    }
+                    //var existingView = existingViews.SingleOrDefault(o => o.Key == view.Key);
+                    //if(existingView != null)
+                    //{
+                    //    view.Id = existingView.Id;
+                    //}
                     await this.dbContext.SystemLandscapeViews.UpsertOneAsync(view);
                 }
                 
@@ -62,15 +62,15 @@ namespace DocuEye.ViewsKeeper.Application.Commands.SaveViewsChanges
                 var keys = request.SystemContextViews.Select(o => o.Key).ToArray();
                 await this.DeleteMissing(request.WorkspaceId, keys, ViewType.SystemContextView);
                 // Get existing views 
-                var existingViews = await this.GetExisting(request.WorkspaceId, ViewType.SystemContextView);
+                //var existingViews = await this.GetExisting(request.WorkspaceId, ViewType.SystemContextView);
                 // Save views
                 foreach (var view in request.SystemContextViews)
                 {
-                    var existingView = existingViews.SingleOrDefault(o => o.Key == view.Key);
-                    if (existingView != null)
-                    {
-                        view.Id = existingView.Id;
-                    }
+                    //var existingView = existingViews.SingleOrDefault(o => o.Key == view.Key);
+                    //if (existingView != null)
+                    //{
+                    //    view.Id = existingView.Id;
+                    //}
                     await this.dbContext.SystemContextViews.UpsertOneAsync(view);
                 }
             }
@@ -87,15 +87,15 @@ namespace DocuEye.ViewsKeeper.Application.Commands.SaveViewsChanges
                 var keys = request.ContainerViews.Select(o => o.Key).ToArray();
                 await this.DeleteMissing(request.WorkspaceId, keys, ViewType.ContainerView);
                 // Get existing views 
-                var existingViews = await this.GetExisting(request.WorkspaceId, ViewType.ContainerView);
+                //var existingViews = await this.GetExisting(request.WorkspaceId, ViewType.ContainerView);
                 // Save views
                 foreach (var view in request.ContainerViews)
                 {
-                    var existingView = existingViews.SingleOrDefault(o => o.Key == view.Key);
-                    if (existingView != null)
-                    {
-                        view.Id = existingView.Id;
-                    }
+                    //var existingView = existingViews.SingleOrDefault(o => o.Key == view.Key);
+                    //if (existingView != null)
+                    //{
+                    //    view.Id = existingView.Id;
+                    //}
                     await this.dbContext.ContainerViews.UpsertOneAsync(view);
                 }
             }
@@ -112,15 +112,15 @@ namespace DocuEye.ViewsKeeper.Application.Commands.SaveViewsChanges
                 var keys = request.ComponentViews.Select(o => o.Key).ToArray();
                 await this.DeleteMissing(request.WorkspaceId, keys, ViewType.ComponentView);
                 // Get existing views 
-                var existingViews = await this.GetExisting(request.WorkspaceId, ViewType.ComponentView);
+                //var existingViews = await this.GetExisting(request.WorkspaceId, ViewType.ComponentView);
                 // Save views
                 foreach (var view in request.ComponentViews)
                 {
-                    var existingView = existingViews.SingleOrDefault(o => o.Key == view.Key);
-                    if (existingView != null)
-                    {
-                        view.Id = existingView.Id;
-                    }
+                    //var existingView = existingViews.SingleOrDefault(o => o.Key == view.Key);
+                    //if (existingView != null)
+                    //{
+                    //   view.Id = existingView.Id;
+                    //}
                     await this.dbContext.ComponentViews.UpsertOneAsync(view);
                 }
             }
@@ -137,15 +137,15 @@ namespace DocuEye.ViewsKeeper.Application.Commands.SaveViewsChanges
                 var keys = request.DeploymentViews.Select(o => o.Key).ToArray();
                 await this.DeleteMissing(request.WorkspaceId, keys, ViewType.DeploymentView);
                 // Get existing views 
-                var existingViews = await this.GetExisting(request.WorkspaceId, ViewType.DeploymentView);
+                //var existingViews = await this.GetExisting(request.WorkspaceId, ViewType.DeploymentView);
                 // Save views
                 foreach (var view in request.DeploymentViews)
                 {
-                    var existingView = existingViews.SingleOrDefault(o => o.Key == view.Key);
-                    if (existingView != null)
-                    {
-                        view.Id = existingView.Id;
-                    }
+                    //var existingView = existingViews.SingleOrDefault(o => o.Key == view.Key);
+                    //if (existingView != null)
+                    //{
+                    //    view.Id = existingView.Id;
+                    //}
                     await this.dbContext.DeploymentViews.UpsertOneAsync(view);
                 }
             }
@@ -162,15 +162,15 @@ namespace DocuEye.ViewsKeeper.Application.Commands.SaveViewsChanges
                 var keys = request.DynamicViews.Select(o => o.Key).ToArray();
                 await this.DeleteMissing(request.WorkspaceId, keys, ViewType.DynamicView);
                 // Get existing views 
-                var existingViews = await this.GetExisting(request.WorkspaceId, ViewType.DynamicView);
+                //var existingViews = await this.GetExisting(request.WorkspaceId, ViewType.DynamicView);
                 // Save views
                 foreach (var view in request.DynamicViews)
                 {
-                    var existingView = existingViews.SingleOrDefault(o => o.Key == view.Key);
-                    if (existingView != null)
-                    {
-                        view.Id = existingView.Id;
-                    }
+                    //var existingView = existingViews.SingleOrDefault(o => o.Key == view.Key);
+                    //if (existingView != null)
+                    //{
+                    //    view.Id = existingView.Id;
+                    //}
                     await this.dbContext.DynamicViews.UpsertOneAsync(view);
                 }
             }
@@ -187,15 +187,15 @@ namespace DocuEye.ViewsKeeper.Application.Commands.SaveViewsChanges
                 var keys = request.FilteredViews.Select(o => o.Key).ToArray();
                 await this.DeleteMissing(request.WorkspaceId, keys, ViewType.FilteredView);
                 // Get existing views 
-                var existingViews = await this.GetExisting(request.WorkspaceId, ViewType.FilteredView);
+                //var existingViews = await this.GetExisting(request.WorkspaceId, ViewType.FilteredView);
                 // Save views
                 foreach (var view in request.FilteredViews)
                 {
-                    var existingView = existingViews.SingleOrDefault(o => o.Key == view.Key);
-                    if (existingView != null)
-                    {
-                        view.Id = existingView.Id;
-                    }
+                    //var existingView = existingViews.SingleOrDefault(o => o.Key == view.Key);
+                    //if (existingView != null)
+                    //{
+                    //    view.Id = existingView.Id;
+                    //}
                     await this.dbContext.FilteredViews.UpsertOneAsync(view);
                 }
             }
@@ -212,15 +212,15 @@ namespace DocuEye.ViewsKeeper.Application.Commands.SaveViewsChanges
                 var keys = request.ImagesViews.Select(o => o.Key).ToArray();
                 await this.DeleteMissing(request.WorkspaceId, keys, ViewType.ImageView);
                 // Get existing views 
-                var existingViews = await this.GetExisting(request.WorkspaceId, ViewType.ImageView);
+                //var existingViews = await this.GetExisting(request.WorkspaceId, ViewType.ImageView);
                 // Save views
                 foreach (var view in request.ImagesViews)
                 {
-                    var existingView = existingViews.SingleOrDefault(o => o.Key == view.Key);
-                    if (existingView != null)
-                    {
-                        view.Id = existingView.Id;
-                    }
+                    // existingView = existingViews.SingleOrDefault(o => o.Key == view.Key);
+                    //if (existingView != null)
+                    //{
+                    //    view.Id = existingView.Id;
+                    //}
                     await this.dbContext.ImageViews.UpsertOneAsync(view);
                 }
             }
@@ -240,11 +240,11 @@ namespace DocuEye.ViewsKeeper.Application.Commands.SaveViewsChanges
                     && o.ViewType == viewType);
         }
 
-        private async Task<IEnumerable<BaseView>> GetExisting(string workspaceId, string viewType)
-        {
-            return await this.dbContext.AllViews
-                    .Find(o => o.WorkspaceId == workspaceId
-                    && o.ViewType == viewType);
-        }
+        //private async Task<IEnumerable<BaseView>> GetExisting(string workspaceId, string viewType)
+        //{
+        //    return await this.dbContext.AllViews
+        //            .Find(o => o.WorkspaceId == workspaceId
+        //            && o.ViewType == viewType);
+        //}
     }
 }
