fafb5669049728970f4c074e1bcd3982f27b4e48
Jacek Zwoliński <jacek.zwolinski@htsoft.pl>
2024-05-04 19:00:33 +0200

    get deployment node elements

---
+++ Source/DocuEye.ModelKeeper.Application/Queries/GetDeploymentNodeModelElements/DeploymentNodeModelElement.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DocuEye.ModelKeeper.Application.Queries.GetDeploymentNodeModelElements
+{
+    public class DeploymentNodeModelElement
+    {
+    }
+}
---
+++ Source/DocuEye.ModelKeeper.Application/Queries/GetDeploymentNodeModelElements/GetDeploymentNodeModelElements.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DocuEye.ModelKeeper.Application.Queries.GetDeploymentNodeModelElements
+{
+    public class GetDeploymentNodeModelElements : IRequest<IEnumerable<DeploymentNodeModelElement>>
+    {
+        public string Id { get; set; }
+
+        public GetDeploymentNodeModelElements(string id)
+        {
+            Id = id;
+        }
+    }
+}
