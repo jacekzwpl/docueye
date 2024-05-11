25170b109e49962b709359f54dbe2b547069fb75
Jacek Zwoliński <jacek.zwolinski@htsoft.pl>
2024-05-11 12:31:44 +0200

    table view

--- Source/DocuEye.Web/client-app/src/views/workspace/relationshipmatrix/RelationshipMatrix.tsx
+++ Source/DocuEye.Web/client-app/src/views/workspace/relationshipmatrix/RelationshipMatrix.tsx
@@ -6,11 +6,17 @@ import DocuEyeApi from "../../../api";
 import { useDispatch } from "react-redux";
 import { setWorkspaceData } from "../../../store/slices/workspace/workspaceSlice";
 import Loader from "../../../components/loader";
+import { Box, Card, CardContent, IconButton, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import SyncAltIcon from '@mui/icons-material/SyncAlt';
 
 export const RelationshipMatrix = () => {
     let { workspaceId } = useParams();
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const [relationships, setRelationships] = useState<DeploymentNodeRelationship[]>([]);
+    const [nodes, setNodes] = useState<any[]>([]);
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -41,6 +47,100 @@ export const RelationshipMatrix = () => {
 
     }, [setIsLoading, setRelationships, dispatch, workspaceId]);
 
+    useEffect(() => {
+        const nodes: any[] = [];
+        relationships.forEach(relationship => {
+            const exisintgSourceNode = nodes.find(node => node.id === relationship.sourceNodeId);
+            const exisintgDestinationNode = nodes.find(node => node.id === relationship.destinationNodeId);
+            if (!exisintgSourceNode) {
+                nodes.push({ id: relationship.sourceNodeId, name: relationship.sourceNodeName });
+            }
+            if (!exisintgDestinationNode) {
+                nodes.push({ id: relationship.destinationNodeId, name: relationship.destinationNodeName });
+            }
+        });
+        nodes.sort((a, b) => {
+            if (a.name < b.name) {
+                return -1;
+            }
+            if (a.name > b.name) {
+                return 1;
+            }
+            return 0;
+        });
+        setNodes(nodes);
+    }, [relationships, setNodes])
 
-    return (<>{isLoading && <Loader />}</>)
+    const getNodesRelation = (sourceNodeId: string, destinationNodeId: string) => {
+        const fromSource = relationships.find(relationship => relationship.sourceNodeId === sourceNodeId && relationship.destinationNodeId === destinationNodeId);
+        const fromDestination = relationships.find(relationship => relationship.sourceNodeId === destinationNodeId && relationship.destinationNodeId === sourceNodeId);
+        return {
+            fromSource,
+            fromDestination
+        }
+    }
+
+    return (
+        <Box padding={2} >
+            <Card variant="outlined" >
+                <CardContent>
+                    <TableContainer sx={{height: 'calc(100vh - 168px)'}}>
+                        <Table stickyHeader sx={{ minWidth: 650}} aria-label="elements table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell sx={{
+                                        position: "sticky",
+                                        left: 0,
+                                        background: "white",
+                                        boxShadow: "5px 2px 5px grey",
+                                        borderRight: "2px solid black"
+                                    }}>Source/Destination</TableCell>
+                                    {nodes.map((node) => (
+                                        <TableCell sx={{minWidth: 100}} key={node.id}><strong>{node.name}</strong></TableCell>
+                                    ))}
+                                    <TableCell sx={{minWidth: 100}} ><strong>aaaa1</strong></TableCell>
+                                    <TableCell sx={{minWidth: 100}} ><strong>aaaa1</strong></TableCell>
+                                    <TableCell sx={{minWidth: 100}} ><strong>aaaa1</strong></TableCell>
+                                    <TableCell sx={{minWidth: 100}} ><strong>aaaa1</strong></TableCell>
+                                    <TableCell sx={{minWidth: 100}} ><strong>aaaa1</strong></TableCell>
+                                    <TableCell sx={{minWidth: 100}} ><strong>aaaa1</strong></TableCell>
+                                    <TableCell sx={{minWidth: 100}} ><strong>aaaa1</strong></TableCell>
+                                    <TableCell sx={{minWidth: 100}} ><strong>aaaa1</strong></TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {nodes.map((sourceNode) => (
+                                    <TableRow key={sourceNode.id}>
+                                        <TableCell sx={{
+                                            position: "sticky",
+                                            left: 0,
+                                            background: "white",
+                                            boxShadow: "5px 2px 5px grey",
+                                            borderRight: "2px solid black"
+                                        }}><strong>{sourceNode.name}</strong></TableCell>
+                                        {nodes.map((destNode) => {
+                                            const dataR = getNodesRelation(sourceNode.id, destNode.id);
+                                            return (<TableCell key={destNode.id}>
+                                                {dataR.fromSource && !dataR.fromDestination && <IconButton><ArrowUpwardIcon /></IconButton>}
+                                                {!dataR.fromSource && dataR.fromDestination && <IconButton><ArrowBackIcon /></IconButton>}
+                                                {dataR.fromSource && dataR.fromDestination && <IconButton><SyncAltIcon /></IconButton>}
+                                            </TableCell>)
+                                        })}
+                                        <TableCell ><strong>aaaa1</strong></TableCell>
+                                        <TableCell ><strong>aaaa1</strong></TableCell>
+                                        <TableCell ><strong>aaaa1</strong></TableCell>
+                                        <TableCell ><strong>aaaa1</strong></TableCell>
+                                        <TableCell ><strong>aaaa1</strong></TableCell>
+                                        <TableCell ><strong>aaaa1</strong></TableCell>
+                                        <TableCell ><strong>aaaa1</strong></TableCell>
+                                        <TableCell ><strong>aaaa1</strong></TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </CardContent>
+            </Card>
+            {isLoading && <Loader />}
+        </Box>)
 }
