aa53257dc21e75b7b86343aaedfdfe8bc0223a9b
Jacek Zwoliński <jacek.zwolinski@htsoft.pl>
2024-06-08 22:15:02 +0200

    WorkspaceKeeper refactor

--- Source/DocuEye.WorkspacesKeeper.Application/Commands/DeleteWorkspace/DeleteWorkspaceCommandHandler.cs
+++ Source/DocuEye.WorkspacesKeeper.Application/Commands/DeleteWorkspace/DeleteWorkspaceCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using DocuEye.WorkspacesKeeper.Application.Events;
+using DocuEye.WorkspacesKeeper.Persistence;
 using MediatR;
 using System.Threading;
 using System.Threading.Tasks;
@@ -8,13 +9,17 @@ namespace DocuEye.WorkspacesKeeper.Application.Commands.DeleteWorkspace
     public class DeleteWorkspaceCommandHandler : IRequestHandler<DeleteWorkspaceCommand>
     {
         private readonly IMediator mediator;
+        private readonly IWorkspacesKeeperDBContext dbContext;
 
-        public DeleteWorkspaceCommandHandler(IMediator mediator)
+        public DeleteWorkspaceCommandHandler(IMediator mediator, IWorkspacesKeeperDBContext dbContext)
         {
             this.mediator = mediator;
+            this.dbContext = dbContext;
         }
         public async Task Handle(DeleteWorkspaceCommand request, CancellationToken cancellationToken)
         {
+            await this.dbContext.ViewConfigurations.DeleteManyAsync(o => o.Id == request.WorkspaceId);
+            await this.dbContext.Workspaces.DeleteManyAsync(o => o.Id == request.WorkspaceId);
             await this.mediator.Publish(new WorkspaceDeletedEvent(request.WorkspaceId), cancellationToken);
         }
     }
