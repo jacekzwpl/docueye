0fe63f64550a867728e351ed4a9ceefe9cf43680
Jacek Zwoliński <jacek.zwolinski@htsoft.pl>
2024-07-13 11:37:35 +0200

    clean

--- Source/DocuEye.Infrastructure.MongoDB/GenericCollection.cs
+++ Source/DocuEye.Infrastructure.MongoDB/GenericCollection.cs
@@ -139,7 +139,7 @@ namespace DocuEye.Infrastructure.MongoDB
         /// <returns>List of founded items</returns>
         public async Task<IEnumerable<T>> Find(Expression<Func<T, bool>> filter, Expression<Func<T, object>> sortValueSelector, bool ascendingSort = true, int? limit = null, int? skip = null)
         {
-            var query = this.collection.Find<T>(filter, new FindOptions() { });
+            var query = this.collection.Find<T>(filter);
             if(ascendingSort)
             {
                 query.SortBy(sortValueSelector);
