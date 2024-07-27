1590a83bc569d1515e013debc72d966f968e44ae
Jacek Zwoliński <jacek.zwolinski@htsoft.pl>
2024-07-27 14:29:49 +0200

    Elements detector

---
+++ Source/DocuEye.WorkspaceImporter.Application.ChangeDetectors.Tests/BaseDetectorsTests.cs
@@ -0,0 +1,37 @@
+using AutoMapper;
+using DocuEye.WorkspaceImporter.Application.Mappings;
+using DocuEye.WorkspaceImporter.Model;
+using MediatR;
+using Moq;
+
+namespace DocuEye.WorkspaceImporter.Application.ChangeDetectors.Tests
+{
+    public class BaseDetectorsTests
+    {
+        protected IMediator mediator;
+        protected IMapper mapper;
+        protected WorkspaceImport import;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            MapperConfiguration config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<ApiToModelPammingProfile>();
+            });
+            this.mapper = config.CreateMapper();
+
+            var mediator = new Mock<IMediator>();
+            mediator.Setup(i => i.Publish(It.IsAny<INotification>(), It.IsAny<CancellationToken>()))
+            .Returns(Task.CompletedTask);
+            this.mediator = mediator.Object;
+
+            this.import = new WorkspaceImport()
+            {
+                Id = Guid.NewGuid().ToString()
+            };
+
+        }
+    }
+}
---
+++ Source/DocuEye.WorkspaceImporter.Application.ChangeDetectors/ElementsChangeDetectorResult.cs
@@ -0,0 +1,13 @@
+﻿using DocuEye.ModelKeeper.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DocuEye.WorkspaceImporter.Application.ChangeDetectors
+{
+    public class ElementsChangeDetectorResult
+    {
+        public List<Element> ElementsToAdd { get; set; } = new List<Element>();
+        public List<Element> ElementsToChange { get; set; } = new List<Element>();
+        public List<Element> ElementsToDelete { get; set; } = new List<Element>();
+    }
+}
