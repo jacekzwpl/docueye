646b5ff721d0849c36d5a18e4eb161cb426d7d87
Jacek Zwoliński <jacek.zwolinski@htsoft.pl>
2024-07-27 10:02:46 +0200

    exploding documentation

---
+++ Source/DocuEye.WorkspaceImporter.Api.Model/Docs/ImageToImport.cs
@@ -0,0 +1,22 @@
+﻿namespace DocuEye.WorkspaceImporter.Api.Model.Docs
+{
+    public class ImageToImport
+    {
+        /// <summary>
+        /// DocuEye ID of the documentation.
+        /// </summary>
+        public string DocumentationId { get; set; } = null!;
+        /// <summary>
+        /// The name of the image.
+        /// </summary>
+        public string? Name { get; set; }
+        /// <summary>
+        /// The (base64 encoded) content of the image.
+        /// </summary>
+        public string? Content { get; set; }
+        /// <summary>
+        /// The image MIME type (e.g. \"image/png\").
+        /// </summary>
+        public string? Type { get; set; }
+    }
+}
