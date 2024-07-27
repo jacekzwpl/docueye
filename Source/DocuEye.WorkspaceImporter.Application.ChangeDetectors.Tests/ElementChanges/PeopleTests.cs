a019b2988a17db147a10afc5dc715a8c89e819f3
Jacek Zwoliński <jacek.zwolinski@htsoft.pl>
2024-07-27 16:39:46 +0200

    people and instances tests

--- Source/DocuEye.WorkspaceImporter.Application.ChangeDetectors.Tests/ElementChanges/PeopleTests.cs
+++ Source/DocuEye.WorkspaceImporter.Application.ChangeDetectors.Tests/ElementChanges/PeopleTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DocuEye.ModelKeeper.Model;
+using DocuEye.WorkspaceImporter.Api.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,19 +13,98 @@ namespace DocuEye.WorkspaceImporter.Application.ChangeDetectors.Tests.ElementCha
         [Test]
         public async Task WhenPersonDoNotExistsThenElementWillBeAdded()
         {
-            Assert.Fail();
+            // Arrange
+            var elementsToImport = new List<ElementToImport>
+            {
+                new ElementToImport
+                {
+                    StructurizrId = "1",
+                    DslId = "person1",
+                    Type = ElementType.Person,
+                    Name = "Person 1"
+                }
+            };
+            var existingElements = new List<Element>();
+            var detector = new ElementsChangeDetector(this.mapper, this.mediator);
+
+            // Act
+            var result = await detector.DetectPeopleChanges("workspace1", import, elementsToImport, existingElements);
+
+            // Assert
+            Assert.That(existingElements.Count(), Is.EqualTo(1));
+            Assert.That(existingElements.First().StructurizrId, Is.EqualTo("1"));
+            Assert.That(existingElements.First().DslId, Is.EqualTo("person1"));
+            Assert.That(existingElements.First().Name, Is.EqualTo("Person 1"));
+            Assert.That(existingElements.First().Type, Is.EqualTo(ElementType.Person));
+
+            Assert.That(result.ElementsToAdd.Count(), Is.EqualTo(1));
         }
 
         [Test]
         public async Task WhenPersonExistsThenElementWillBeUpdated()
         {
-            Assert.Fail();
+            // Arrange
+            var elementsToImport = new List<ElementToImport>
+            {
+                new ElementToImport
+                {
+                    StructurizrId = "1",
+                    DslId = "person1",
+                    Type = ElementType.Person,
+                    Name = "Person 1 changed"
+                }
+            };
+            var existingElements = new List<Element>
+            {
+                new Element
+                {
+                    Id = "personID1",
+                    StructurizrId = "1",
+                    DslId = "person1",
+                    Type = ElementType.Person,
+                    Name = "Person 1"
+                }
+            };
+            var detector = new ElementsChangeDetector(this.mapper, this.mediator);
+
+            // Act
+            var result = await detector.DetectPeopleChanges("workspace1", import, elementsToImport, existingElements);
+
+            // Assert
+            Assert.That(existingElements.Count(), Is.EqualTo(1));
+            Assert.That(existingElements.First().StructurizrId, Is.EqualTo("1"));
+            Assert.That(existingElements.First().DslId, Is.EqualTo("person1"));
+            Assert.That(existingElements.First().Name, Is.EqualTo("Person 1 changed"));
+            Assert.That(existingElements.First().Type, Is.EqualTo(ElementType.Person));
+            Assert.That(existingElements.First().Id, Is.EqualTo("personID1"));
         }
 
         [Test]
         public async Task WhenPersonExistsButIsNotInImportThenElementWillBeDeleted()
         {
-            Assert.Fail();
+            // Arrange
+            var elementsToImport = new List<ElementToImport>();
+            var existingElements = new List<Element>
+            {
+                new Element
+                {
+                    Id = "personID1",
+                    StructurizrId = "1",
+                    DslId = "person1",
+                    Type = ElementType.Person,
+                    Name = "Person 1"
+                }
+            };
+            var detector = new ElementsChangeDetector(this.mapper, this.mediator);
+
+            // Act
+            var result = await detector.DetectPeopleChanges("workspace1", import, elementsToImport, existingElements);
+
+            // Assert
+            Assert.That(existingElements.Count(), Is.EqualTo(0));
+
+            Assert.That(result.ElementsToDelete.Count(), Is.EqualTo(1));
+            Assert.That(result.ElementsToDelete.First(), Is.EqualTo("personID1"));
         }
     }
 }
--- Source/DocuEye.WorkspaceImporter.Application.ChangeDetectors.Tests/ElementChanges/SoftwareSystemInstancesTests.cs
+++ Source/DocuEye.WorkspaceImporter.Application.ChangeDetectors.Tests/ElementChanges/SoftwareSystemInstancesTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DocuEye.ModelKeeper.Model;
+using DocuEye.WorkspaceImporter.Api.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,19 +13,202 @@ namespace DocuEye.WorkspaceImporter.Application.ChangeDetectors.Tests.ElementCha
         [Test]
         public async Task WhenSoftwareSystemInstanceDoNotExistsThenElementWillBeAdded()
         {
-            Assert.Fail();
+            // Arrange
+            var elementsToImport = new List<ElementToImport>
+            {
+                new ElementToImport
+                {
+                    StructurizrId = "1",
+                    DslId = "softwaresystem1",
+                    Type = ElementType.SoftwareSystem,
+                    Name = "Software System 1"
+                },
+                new ElementToImport
+                {
+                    StructurizrId = "2",
+                    DslId = "deploymentNode1",
+                    Type = ElementType.DeploymentNode,
+                    Name = "Deployment Node 1"
+                },
+                new ElementToImport
+                {
+                    StructurizrId = "3",
+                    StructurizrInstanceOfId = "1",
+                    StructurizrParentId = "2",
+                    DslId = "softwaresysteminstance1",
+                    Type = ElementType.SoftwareSystemInstance
+                }
+            };
+            var existingElements = new List<Element>
+            {
+                new Element
+                {
+                    Id = "ID1",
+                    StructurizrId = "1",
+                    DslId = "softwaresystem1",
+                    Type = ElementType.SoftwareSystem,
+                    Name = "Software System 1"
+                },
+                new Element
+                {
+                    Id = "ID2",
+                    StructurizrId = "2",
+                    DslId = "deploymentNode1",
+                    Type = ElementType.DeploymentNode,
+                    Name = "Deployment Node 1"
+                }
+            };
+            var detector = new ElementsChangeDetector(this.mapper, this.mediator);
+
+            // Act
+            var result = await detector.DetectSoftwareSystemInstancesChanges("workspace1", this.import, elementsToImport, existingElements);
+
+            // Assert
+            Assert.That(existingElements.Count(), Is.EqualTo(3));
+            Assert.That(existingElements.Last().StructurizrId, Is.EqualTo("3"));
+            Assert.That(existingElements.Last().DslId, Is.EqualTo("softwaresysteminstance1"));
+            Assert.That(existingElements.Last().Name, Is.EqualTo("Software System 1"));
+            Assert.That(existingElements.Last().Type, Is.EqualTo(ElementType.SoftwareSystemInstance));
+            Assert.That(existingElements.Last().ParentId, Is.EqualTo(existingElements[1].Id));
+            Assert.That(existingElements.Last().InstanceOfId, Is.EqualTo(existingElements[0].Id));
+
+            Assert.That(result.ElementsToAdd.Count(), Is.EqualTo(1));
         }
 
         [Test]
         public async Task WhenSoftwareSystemInstanceExistsThenElementWillBeUpdated()
         {
-            Assert.Fail();
+            // Arrange
+            var elementsToImport = new List<ElementToImport>
+            {
+                new ElementToImport
+                {
+                    StructurizrId = "1",
+                    DslId = "softwaresystem1",
+                    Type = ElementType.SoftwareSystem,
+                    Name = "Software System 1"
+                },
+                new ElementToImport
+                {
+                    StructurizrId = "2",
+                    DslId = "deploymentNode1",
+                    Type = ElementType.DeploymentNode,
+                    Name = "Deployment Node 1"
+                },
+                new ElementToImport
+                {
+                    StructurizrId = "3",
+                    StructurizrInstanceOfId = "1",
+                    StructurizrParentId = "2",
+                    DslId = "softwaresysteminstance1",
+                    Type = ElementType.SoftwareSystemInstance
+                }
+            };
+            var existingElements = new List<Element>
+            {
+                new Element
+                {
+                    Id = "ID1",
+                    StructurizrId = "1",
+                    DslId = "softwaresystem1",
+                    Type = ElementType.SoftwareSystem,
+                    Name = "Software System 1"
+                },
+                new Element
+                {
+                    Id = "ID2",
+                    StructurizrId = "2",
+                    DslId = "deploymentNode1",
+                    Type = ElementType.DeploymentNode,
+                    Name = "Deployment Node 1"
+                },
+                new Element
+                {
+                    Id = "ID3",
+                    StructurizrId = "3",
+                    ParentId = "ID2",
+                    InstanceOfId = "ID1",
+                    DslId = "softwaresysteminstance1",
+                    Type = ElementType.SoftwareSystemInstance,
+                    Name = "Software System 1"
+                }
+            };
+            var detector = new ElementsChangeDetector(this.mapper, this.mediator);
+
+            // Act
+            var result = await detector.DetectSoftwareSystemInstancesChanges("workspace1", this.import, elementsToImport, existingElements);
+
+            // Assert
+            Assert.That(existingElements.Count(), Is.EqualTo(3));
+            Assert.That(existingElements.Last().StructurizrId, Is.EqualTo("3"));
+            Assert.That(existingElements.Last().DslId, Is.EqualTo("softwaresysteminstance1"));
+            Assert.That(existingElements.Last().Name, Is.EqualTo("Software System 1"));
+            Assert.That(existingElements.Last().Type, Is.EqualTo(ElementType.SoftwareSystemInstance));
+            Assert.That(existingElements.Last().ParentId, Is.EqualTo(existingElements[1].Id));
+            Assert.That(existingElements.Last().InstanceOfId, Is.EqualTo(existingElements[0].Id));
+
         }
 
         [Test]
         public async Task WhenSoftwareSystemInstanceExistsButIsNotInImportThenElementWillBeDeleted()
         {
-            Assert.Fail();
+            // Arrange
+            var elementsToImport = new List<ElementToImport>
+            {
+                new ElementToImport
+                {
+                    StructurizrId = "1",
+                    DslId = "softwaresystem1",
+                    Type = ElementType.SoftwareSystem,
+                    Name = "Software System 1"
+                },
+                new ElementToImport
+                {
+                    StructurizrId = "2",
+                    DslId = "deploymentNode1",
+                    Type = ElementType.DeploymentNode,
+                    Name = "Deployment Node 1"
+                }
+            };
+            var existingElements = new List<Element>
+            {
+                new Element
+                {
+                    Id = "ID1",
+                    StructurizrId = "1",
+                    DslId = "softwaresystem1",
+                    Type = ElementType.SoftwareSystem,
+                    Name = "Software System 1"
+                },
+                new Element
+                {
+                    Id = "ID2",
+                    StructurizrId = "2",
+                    DslId = "deploymentNode1",
+                    Type = ElementType.DeploymentNode,
+                    Name = "Deployment Node 1"
+                },
+                new Element
+                {
+                    Id = "ID3",
+                    StructurizrId = "3",
+                    ParentId = "ID2",
+                    InstanceOfId = "ID1",
+                    DslId = "softwaresysteminstance1",
+                    Type = ElementType.SoftwareSystemInstance,
+                    Name = "Software System 1"
+                }
+            };
+            var detector = new ElementsChangeDetector(this.mapper, this.mediator);
+
+            // Act
+            var result = await detector.DetectSoftwareSystemInstancesChanges("workspace1", this.import, elementsToImport, existingElements);
+
+            // Assert
+            Assert.That(existingElements.Count(), Is.EqualTo(2));
+
+            Assert.That(result.ElementsToDelete.Count(), Is.EqualTo(1));
+            Assert.That(result.ElementsToDelete.First(), Is.EqualTo("ID3"));
         }
     }
 }
