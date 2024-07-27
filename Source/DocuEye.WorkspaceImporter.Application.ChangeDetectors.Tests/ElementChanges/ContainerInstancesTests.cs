f617d67b034d233e5707efcb6a5374097527b978
Jacek Zwoliński <jacek.zwolinski@htsoft.pl>
2024-07-27 14:39:00 +0200

    basic tests sceleton

---
+++ Source/DocuEye.WorkspaceImporter.Application.ChangeDetectors.Tests/ElementChanges/ContainerInstancesTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocuEye.WorkspaceImporter.Application.ChangeDetectors.Tests.ElementChanges
+{
+    public class ContainerInstancesTests : BaseDetectorsTests
+    {
+        [Test]
+        public async Task WhenContainerInstanceDoNotExistsThenElementWillBeAdded()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public async Task WhenContainerInstanceExistsThenElementWillBeUpdated()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public async Task WhenContainerInstanceExistsButIsNotInImportThenElementWillBeDeleted()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ Source/DocuEye.WorkspaceImporter.Application.ChangeDetectors.Tests/ElementChanges/InfrastructureNodesTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocuEye.WorkspaceImporter.Application.ChangeDetectors.Tests.ElementChanges
+{
+    public class InfrastructureNodesTests : BaseDetectorsTests
+    {
+        [Test]
+        public async Task WhenInfrastructureNodeDoNotExistsThenElementWillBeAdded()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public async Task WhenInfrastructureNodeExistsThenElementWillBeUpdated()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public async Task WhenInfrastructureNodeExistsButIsNotInImportThenElementWillBeDeleted()
+        {
+            Assert.Fail();
+        }
+    }
+}
