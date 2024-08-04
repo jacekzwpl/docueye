d467b816b4456563bdb3a45f060f516a407ee68d
Jacek Zwoliński <jacek.zwolinski@htsoft.pl>
2024-08-04 09:27:10 +0200

    detect view ids test

---
+++ Source/DocuEye.WorkspaceImporter.Application.ChangeDetectors.Tests/ViewChanges/DetectViewsIdsTests.cs
@@ -0,0 +1,134 @@
+﻿using DocuEye.ModelKeeper.Model;
+using DocuEye.ViewsKeeper.Model;
+using DocuEye.WorkspaceImporter.Api.Model.Views;
+
+namespace DocuEye.WorkspaceImporter.Application.ChangeDetectors.Tests.ViewChanges
+{
+    public class DetectViewsIdsTests : BaseDetectorsTests
+    {
+        [Test]
+        public void WhenNewViewIsDefindedThenIdIsGenerated() { 
+        
+            // Arrange
+            var viewsToImport = new List<ViewToImport>()
+            {
+                new ViewToImport()
+                {
+                    Key = "view1"
+                }
+            };
+            var existingViews = new List<BaseView>();
+            var existingElements = new List<Element>();
+            var detector = new ViewsChangeDetector(this.mapper, this.mediator);
+
+            // Act
+            var (viewsIdsMap, elementsDiagrams) = detector.DetectViewsIds(viewsToImport, existingViews, existingElements);
+
+            // Assert
+            Assert.That(viewsIdsMap.Count, Is.EqualTo(1));
+            Assert.That(viewsIdsMap.First().Key, Is.EqualTo("view1"));
+        
+        }
+
+        [Test]
+        public void WhenExistingViewIsDefindedThenIdIsTakenFromExistingView() { 
+        
+            // Arrange
+            var viewsToImport = new List<ViewToImport>()
+            {
+                new ViewToImport()
+                {
+                    Key = "view1"
+                }
+            };
+            var existingViews = new List<BaseView>()
+            {
+                new BaseView()
+                {
+                    Id = "viewid1",
+                    Key = "view1"
+                }
+            };
+            var existingElements = new List<Element>();
+            var detector = new ViewsChangeDetector(this.mapper, this.mediator);
+
+            // Act
+            var (viewsIdsMap, elementsDiagrams) = detector.DetectViewsIds(viewsToImport, existingViews, existingElements);
+
+            // Assert
+            Assert.That(viewsIdsMap.Count, Is.EqualTo(1));
+            Assert.That(viewsIdsMap.First().Key, Is.EqualTo("view1"));
+            Assert.That(viewsIdsMap.First().Value, Is.EqualTo("viewid1"));
+        }
+
+        [Test]
+        public void WhenNewViewHasContextElementThenElementIsAddedToElementsDiagrams() { 
+            
+            // Arrange
+            var viewsToImport = new List<ViewToImport>()
+            {
+                new ViewToImport()
+                {
+                    Key = "view1",
+                    StructurizrElementId = "element1"
+                }
+            };
+            var existingViews = new List<BaseView>();
+            var existingElements = new List<Element>()
+            {
+                new Element()
+                {
+                    Id = "element1",
+                    StructurizrId = "element1"
+                }
+            };
+            var detector = new ViewsChangeDetector(this.mapper, this.mediator);
+
+            // Act
+            var (viewsIdsMap, elementsDiagrams) = detector.DetectViewsIds(viewsToImport, existingViews, existingElements);
+
+            // Assert
+            Assert.That(elementsDiagrams.Count, Is.EqualTo(1));
+            Assert.That(elementsDiagrams.First().Key, Is.EqualTo("element1"));
+            Assert.That(elementsDiagrams.First().Value, Is.EqualTo(viewsIdsMap["view1"]));
+        }
+
+        [Test]
+        public void WhenExistingViewHasContextElementThenElementIsAddedToElementsDiagrams() { 
+        
+            // Arrange
+            var viewsToImport = new List<ViewToImport>()
+            {
+                new ViewToImport()
+                {
+                    Key = "view1",
+                    StructurizrElementId = "element1"
+                }
+            };
+            var existingViews = new List<BaseView>() {
+                new BaseView()
+                {
+                    Id = "viewid1",
+                    Key = "view1"
+                }
+            };
+            var existingElements = new List<Element>()
+            {
+                new Element()
+                {
+                    Id = "element1",
+                    StructurizrId = "element1"
+                }
+            };
+            var detector = new ViewsChangeDetector(this.mapper, this.mediator);
+
+            // Act
+            var (viewsIdsMap, elementsDiagrams) = detector.DetectViewsIds(viewsToImport, existingViews, existingElements);
+
+            // Assert
+            Assert.That(elementsDiagrams.Count, Is.EqualTo(1));
+            Assert.That(elementsDiagrams.First().Key, Is.EqualTo("element1"));
+            Assert.That(elementsDiagrams.First().Value, Is.EqualTo("viewid1"));
+        }
+    }
+}
