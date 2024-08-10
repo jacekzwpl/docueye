dce2458b76a390d03dc68901be580515b9c6411a
Jacek Zwoliński <jacek.zwolinski@htsoft.pl>
2024-08-10 11:40:11 +0200

    Test class

---
+++ Source/DocuEye.WorkspaceImporter.Application.Tests/Commands/ImportElementsCommandHandlerTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocuEye.WorkspaceImporter.Application.Tests.Commands
+{
+    public class ImportElementsCommandHandlerTests : BaseCommandTests
+    {
+        [Test]
+        public async Task WhenImportElementsAndImportDoNotExistsThenReturnStatusFalse()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public async Task WhenImportElementsAndImportExistsButIsFinishedThenReturnStatusFalse()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public async Task WhenImportElementsThenReturnStatusTrue()
+        {
+            Assert.Fail();
+        }
+    }
+}
