b570274b24c0c522f6b2efd7f170fa5ecadf15e4
Jacek Zwoliński <jacek.zwolinski@htsoft.pl>
2024-11-11 16:00:40 +0100

    import flow in cli

--- Source/DocuEye.CLI.Application/Services/ImportWorkspace/ImportWorkspaceService.cs
+++ Source/DocuEye.CLI.Application/Services/ImportWorkspace/ImportWorkspaceService.cs
@@ -1,15 +1,21 @@
 ﻿using AutoMapper;
 using DocuEye.CLI.ApiClient;
+using DocuEye.ModelKeeper.Model;
 using DocuEye.Structurizr.Model;
 using DocuEye.Structurizr.Model.Exploders;
 using DocuEye.WorkspaceImporter.Api.Model;
 using DocuEye.WorkspaceImporter.Api.Model.Docs;
+using DocuEye.WorkspaceImporter.Api.Model.Elements;
+using DocuEye.WorkspaceImporter.Api.Model.Relationships;
+using DocuEye.WorkspaceImporter.Api.Model.ViewConfiguration;
 using DocuEye.WorkspaceImporter.Api.Model.Views;
 using DocuEye.WorkspaceImporter.Api.Model.Workspace;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
@@ -117,14 +123,92 @@ namespace DocuEye.CLI.Application.Services.ImportWorkspace
                 });
             });
 
-            
-
-
-
-            this.logger.LogInformation("Importing workspace");
-            
+            // Set workspace id if not set
+            if (string.IsNullOrEmpty(parameters.WorkspaceId))
+            {
+                parameters.WorkspaceId = Guid.NewGuid().ToString();
+            }
             // Start import
-            var result = await this.apiClient.ImportStart( new ImportWorkspaceStartRequest()
+            if (!await this.RunImportStart(parameters, workspaceData))
+            {
+                return false;
+            }
+
+            //Import view configuration
+            if(!await this.RunImportViewConfiguration(parameters, viewConfiguration))
+            {
+                return false;
+            }
+
+            //Import elements
+            if(!await this.RunImportElements(parameters, elements))
+            {
+                return false;
+            }
+
+            //Import relationships
+            if(!await this.RunImportRelationships(parameters, relationships))
+            {
+                return false;
+            }
+
+            //Import views
+            if(!await this.RunImportViews(parameters, viewsToImport))
+            {
+                return false;
+            }
+
+            // Clear doc items
+            if(!await this.RunImportClearDocItems(parameters))
+            {
+                return false;
+            }
+
+            // Import documentation
+            if(!await this.RunImportDocumentation(parameters, documentationsToImport))
+            {
+                return false;
+            }
+
+            // Import images 
+            if(!await this.RunImportImage(parameters, imageToImport))
+            {
+                return false;
+            }
+
+            // Import decisions
+            if(!await this.RunImportDecisions(parameters, decisionToImport))
+            {
+                return false;
+            }
+
+            // Import decision links
+            if(!await this.RunImportDecisionLinks(parameters, decisionToImport))
+            {
+                return false;
+            }
+
+            // Clear decisions
+            if(!await this.RunImportClearDecisions(parameters))
+            {
+                return false;
+            }
+
+            // Finish import
+            if(!await this.RunImportFinish(parameters))
+            {
+                return false;
+            }
+
+            this.logger.LogInformation("Workspace ID = {0}", parameters.WorkspaceId);
+            return true;        
+        }
+
+        private async Task<bool> RunImportStart(ImportWorkspaceParameters parameters, StructurizrWorkspace workspaceData)
+        {
+            string stepName = "Import start";
+            this.logger.LogInformation("Running Step: {0}", stepName);
+            var result = await this.apiClient.ImportStart(new ImportWorkspaceStartRequest()
             {
                 ImportKey = parameters.ImportKey,
                 SourceLink = parameters.SourceLink,
@@ -135,182 +219,207 @@ namespace DocuEye.CLI.Application.Services.ImportWorkspace
                 AccessRules = this.mapper.Map<IEnumerable<WorkspaceAccessRuleToImport>>(workspaceData.Configuration?.Users)
             });
 
-            this.LogImportStepResult(result, "Import start");
-            if(!result.IsSuccess)
-            {
-                return false;
-            }
+            this.LogImportStepResult(result, stepName);
+            return result.IsSuccess;
+        }
 
-            //set workspaceId
-            var workspaceId = result.WorkspaceId;
-
-            //Import view configuration
-            var result2 = await this.apiClient.ImportViewConfiguration(new ImportViewConfigurationRequest()
+        private async Task<bool> RunImportViewConfiguration(ImportWorkspaceParameters parameters, ViewConfigurationToImport viewConfiguration)
+        {
+            string stepName = "Import view configuration";
+            this.logger.LogInformation("Running Step: {0}", stepName);
+            var result = await this.apiClient.ImportViewConfiguration(new ImportViewConfigurationRequest()
             {
                 ImportKey = parameters.ImportKey,
-                WorkspaceId = workspaceId,
+                WorkspaceId = parameters.WorkspaceId,
                 ViewConfiguration = viewConfiguration
             });
 
-            this.LogImportStepResult(result2, "Import view configuration");
-            if (!result2.IsSuccess)
-            {
-                return false;
-            }
+            this.LogImportStepResult(result, stepName);
+            return result.IsSuccess;
+        }
 
-            //Import elements
-            var resutlt3 = await this.apiClient.ImportElements(new ImportElementsRequest()
+        private async Task<bool> RunImportElements(ImportWorkspaceParameters parameters, IEnumerable<ElementToImport> elements)
+        {
+            string stepName = "Import elements";
+            this.logger.LogInformation("Running Step: {0}", stepName);
+            var result = await this.apiClient.ImportElements(new ImportElementsRequest()
             {
                 ImportKey = parameters.ImportKey,
-                WorkspaceId = workspaceId,
+                WorkspaceId = parameters.WorkspaceId,
                 Elements = elements
             });
+            this.LogImportStepResult(result, stepName);
+            return result.IsSuccess;
+        }
 
-            this.LogImportStepResult(resutlt3, "Import elements");
-            if (!resutlt3.IsSuccess)
-            {
-                return false;
-            }
-
-            //Import relationships
-            var result4 = await this.apiClient.ImportRelationships(new ImportRelationshipsRequest()
+        private async Task<bool> RunImportRelationships(ImportWorkspaceParameters parameters, IEnumerable<RelationshipToImport> relationships)
+        {
+            string stepName = "Import elements";
+            this.logger.LogInformation("Running Step: {0}", stepName);
+            var result = await this.apiClient.ImportRelationships(new ImportRelationshipsRequest()
             {
                 ImportKey = parameters.ImportKey,
-                WorkspaceId = workspaceId,
+                WorkspaceId = parameters.WorkspaceId,
                 Relationships = relationships
             });
+            this.LogImportStepResult(result, stepName);
+            return result.IsSuccess;
+        }
 
-            this.LogImportStepResult(result4, "Import relationships");
-            if (!result4.IsSuccess)
-            {
-                return false;
-            }
-
-            //Import views
-            var result5 = await this.apiClient.ImportViews(new ImportViewsRequest()
+        private async Task<bool> RunImportViews(ImportWorkspaceParameters parameters, List<ViewToImport> viewsToImport)
+        {
+            string stepName = "Import views";
+            this.logger.LogInformation("Running Step: {0}", stepName);
+            var result = await this.apiClient.ImportViews(new ImportViewsRequest()
             {
                 ImportKey = parameters.ImportKey,
-                WorkspaceId = workspaceId,
+                WorkspaceId = parameters.WorkspaceId,
                 Views = viewsToImport
             });
+            this.LogImportStepResult(result, stepName);
+            return result.IsSuccess;
+        }
 
-            this.LogImportStepResult(result5, "Import views");
-            if (!result5.IsSuccess)
-            {
-                return false;
-            }
-
-            // Clear doc items
-            var result6 = await this.apiClient.ImportClearDocItems(new ImportClearDocItemsRequest()
+        private async Task<bool> RunImportClearDocItems(ImportWorkspaceParameters parameters)
+        {
+            string stepName = "Clear documentation items";
+            this.logger.LogInformation("Running Step: {0}", stepName);
+            var result = await this.apiClient.ImportClearDocItems(new ImportClearDocItemsRequest()
             {
                 ImportKey = parameters.ImportKey,
-                WorkspaceId = workspaceId
+                WorkspaceId = parameters.WorkspaceId
             });
+            this.LogImportStepResult(result, stepName);
+            return result.IsSuccess;
+        }
 
-            this.LogImportStepResult(result6, "Clear doc items");
-            if (!result6.IsSuccess)
-            {
-                return false;
-            }
+        private async Task<bool> RunImportDocumentation(ImportWorkspaceParameters parameters, List<DocumentationToImport> documentationsToImport)
+        {
+            string stepName = "Import documentation";
+            this.logger.LogInformation("Running Step: {0}", stepName);
 
-            // Import documentation
             foreach (var documentation in documentationsToImport)
             {
-                var result7 = await this.apiClient.ImportDocumentation(new ImportDocumentationRequest()
+                this.logger.LogInformation("Imort documentation ID = {0}", documentation.Id);
+                var result = await this.apiClient.ImportDocumentation(new ImportDocumentationRequest()
                 {
                     ImportKey = parameters.ImportKey,
-                    WorkspaceId = workspaceId,
+                    WorkspaceId = parameters.WorkspaceId,
                     Documentation = documentation
                 });
 
-                this.LogImportStepResult(result7, "Import documentation");
-                if (!result7.IsSuccess)
+                
+                if (!result.IsSuccess)
                 {
+                    this.LogImportStepResult(result, stepName);
                     return false;
                 }
             }
+            this.logger.LogInformation("{0} finished", stepName);
+            return true;
+        }
 
-
-
-            // Import images 
-            foreach (var image in imageToImport)
+        private async Task<bool> RunImportImage(ImportWorkspaceParameters parameters, List<ImageToImport> imagesToImport)
+        {
+            string stepName = "Import Images";
+            this.logger.LogInformation("Running Step: {0}", stepName);
+            foreach (var image in imagesToImport)
             {
-                var result8 = await this.apiClient.ImportImage(new ImportImageRequest()
+                var result = await this.apiClient.ImportImage(new ImportImageRequest()
                 {
                     ImportKey = parameters.ImportKey,
-                    WorkspaceId = workspaceId,
+                    WorkspaceId = parameters.WorkspaceId,
                     Image = image
                 });
 
-                this.LogImportStepResult(result8, "Import image");
-                if (!result8.IsSuccess)
+                
+                if (!result.IsSuccess)
                 {
+                    this.LogImportStepResult(result, stepName);
                     return false;
                 }
             }
+            this.logger.LogInformation("{0} finished", stepName);
+            return true;
+        }
 
-            // Import decisions
-            foreach (var decision in decisionToImport)
+        public async Task<bool> RunImportDecisions(ImportWorkspaceParameters parameters, List<DecisionToImport> decisionsToImport)
+        {
+            string stepName = "Import decisions";
+            this.logger.LogInformation("Running Step: {0}", stepName);
+            foreach (var decision in decisionsToImport)
             {
-                var result9 = await this.apiClient.ImportDecision(new ImportDecisionRequest()
+                var result = await this.apiClient.ImportDecision(new ImportDecisionRequest()
                 {
                     ImportKey = parameters.ImportKey,
-                    WorkspaceId = workspaceId,
+                    WorkspaceId = parameters.WorkspaceId,
                     Decision = decision
                 });
 
-                this.LogImportStepResult(result9, "Import decision");
-                if (!result9.IsSuccess)
+                
+                if (!result.IsSuccess)
                 {
+                    this.LogImportStepResult(result, stepName);
                     return false;
                 }
             }
+            this.logger.LogInformation("{0} finished", stepName);
+            return true;
+        }
 
-            // Import decision links
-            foreach (var decision in decisionToImport)
+        public async Task<bool> RunImportDecisionLinks(ImportWorkspaceParameters parameters, List<DecisionToImport> decisionsToImport)
+        {
+            string stepName = "Import decisions links";
+            this.logger.LogInformation("Running Step: {0}", stepName);
+            foreach (var decision in decisionsToImport)
             {
                 var result10 = await this.apiClient.ImportDecisionLinks(new ImportDecisionsLinksRequest()
                 {
                     ImportKey = parameters.ImportKey,
-                    WorkspaceId = workspaceId,
+                    WorkspaceId = parameters.WorkspaceId,
                     DecisionDslId = decision.StrucuturizrId,
                     DocumentationId = decision.DocumentationId,
                     DecisionsLinks = decision.Links ?? Enumerable.Empty<DecisionLinkToImport>()
                 });
 
-                this.LogImportStepResult(result10, "Import decision links");
+                this.LogImportStepResult(result10, stepName);
                 if (!result10.IsSuccess)
                 {
                     return false;
                 }
             }
-
-            // Clear decisions
-            var result11 = await this.apiClient.ImportClearDecisions(new ImportClearDecisionsRequest()
-            {
-                ImportKey = parameters.ImportKey,
-                WorkspaceId = workspaceId
-            });
-
-            this.LogImportStepResult(result11, "Clear decisions");
-            if (!result11.IsSuccess)
-            {
-                return false;
-            }
-
-            // Finish import
-            var result12 = await this.apiClient.ImportFinish(new ImportFinalizeRequest()
-            {
-                ImportKey = parameters.ImportKey,
-                WorkspaceId = workspaceId
-            });
-
-            this.LogImportStepResult(result12, "Import finish");
-            this.logger.LogInformation("Workspace ID = {0}", result.WorkspaceId);
-            return result12.IsSuccess;            
-
-            
+            this.logger.LogInformation("{0} finished", stepName);
+            return true;
         }
+
+        private async Task<bool> RunImportClearDecisions(ImportWorkspaceParameters parameters)
+        {
+            string stepName = "Clear decisions";
+            this.logger.LogInformation("Running Step: {0}", stepName);
+            var result = await this.apiClient.ImportClearDecisions(new ImportClearDecisionsRequest()
+            {
+                ImportKey = parameters.ImportKey,
+                WorkspaceId = parameters.WorkspaceId
+            });
+
+            this.LogImportStepResult(result, stepName);
+            return result.IsSuccess;
+        }
+
+        private async Task<bool> RunImportFinish(ImportWorkspaceParameters parameters)
+        {
+            string stepName = "Import finish";
+            this.logger.LogInformation("Running Step: {0}", stepName);
+            var result = await this.apiClient.ImportFinish(new ImportFinalizeRequest()
+            {
+                ImportKey = parameters.ImportKey,
+                WorkspaceId = parameters.WorkspaceId
+            });
+
+            this.LogImportStepResult(result, stepName);
+            return result.IsSuccess;
+        }
+
 
         private void LogImportStepResult(ImportWorkspaceResponse result, string stepName)
         {
