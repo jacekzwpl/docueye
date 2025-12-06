963a13cff9358b8d43cdbbc99a510c48d3022fec
Jacek Zwoliński <jacek.zwolinski@htsoft.pl>
2025-12-06 15:01:40 +0100

    relationship in view map

---
+++ Source/DocuEye.Structurizr.Json.Model.Maps/StructurizrJsonRelationshipViewMap.cs
@@ -0,0 +1,22 @@
+﻿using DocuEye.WorkspaceImporter.Api.Model.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DocuEye.Structurizr.Json.Model.Maps
+{
+    public static class StructurizrJsonRelationshipViewMap
+    {
+        public static RelationshipInViewToImport ToRelationshipInViewToImport(this StructurizrJsonRelationshipView source)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            return new RelationshipInViewToImport
+            {
+                StructurizrId = source.Id ?? string.Empty,
+                Description = source.Description,
+                Response = source.Response,
+                Order = source.Order
+            };
+        }
+    }
+}
