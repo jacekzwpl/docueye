107a110a66e22d9091fc2d30472fb976fc3e4038
Jacek Zwoliński <jacek.zwolinski@htsoft.pl>
2025-12-07 17:28:44 +0100

    all maps

---
+++ Source/DocuEye.WorkspacesKeeper.Model.Maps.Tests/WorkspaceMapTests.cs
@@ -0,0 +1,27 @@
+﻿using DocuEye.Infrastructure.Tests.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocuEye.WorkspacesKeeper.Model.Maps.Tests
+{
+    public class WorkspaceMapTests
+    {
+        [Test]
+        public void Mapping_Workspace_To_FoundedWorkspace_Works()
+        {
+            var source = new Workspace
+            {
+                Id = "ws-123",
+                Name = "Workspace Name",
+                Description = "Workspace Description"
+            };
+            var result = source.MapToFoundedWorkspace();
+            MappingAssert.AssertMapped(
+                source, result,
+                ignoreDestProps: Array.Empty<string>());
+        }
+    }
+}
---
+++ Source/DocuEye.WorkspacesKeeper.Model.Maps/WorkspaceMap.cs
@@ -0,0 +1,20 @@
+﻿using DocuEye.WorkspacesKeeper.Application.Model;
+using System;
+
+namespace DocuEye.WorkspacesKeeper.Model.Maps
+{
+    public static class WorkspaceMap
+    {
+        public static FoundedWorkspace MapToFoundedWorkspace(this Workspace source)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            var dest = new FoundedWorkspace
+            {
+                Id = source.Id,
+                Name = source.Name,
+                Description = source.Description
+            };
+            return dest;
+        }
+    }
+}
