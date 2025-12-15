fb116ef1dd3abddb6d71a596224e0d200bd67cc0
Jacek Zwoliński <jacek.zwolinski@htsoft.pl>
2025-12-15 17:30:46 +0100

    client app with https

--- Source/DocuEye.Web/client-app/vite.config.ts
+++ Source/DocuEye.Web/client-app/vite.config.ts
@@ -1,8 +1,22 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import fs from "fs";
+
+console.log('mode:', process.env.NODE_ENV);
+
+Object.assign(process.env, loadEnv(process.env.NODE_ENV ?? "development", process.cwd(), ""));
+
+const options = process.env.NODE_ENV === "development" ? {
+  key: fs.readFileSync(process.env.SSL_KEY_FILE ?? ""),
+  cert: fs.readFileSync(process.env.SSL_CRT_FILE ?? ""),
+} : {};
 
 export default defineConfig({
+  server: {
+    https: options,
+    port: 3000,
+  },
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
