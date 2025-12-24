912623925c7e06e0823033ad135b72bf3ae9531e
Jacek Zwoliński <jacek.zwolinski@htsoft.pl>
2025-12-24 14:15:51 +0100

    working on linter poc

---
+++ Source/DocuEye.Linter.Model/LinterRule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DocuEye.Linter.Model
+{
+    public class LinterRule
+    {
+        public string Key { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public string Severity { get; set; }
+        public string Type { get; set; }
+        public string? Expression { get; set; }
+        public string? HelpLink { get; set; }
+        public bool Enabled { get; set; } = true;
+    }
+}
