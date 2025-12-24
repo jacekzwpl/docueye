489cafa469ad1ff32fa1f60596ccab528bf67d15
Jacek Zwoliński <jacek.zwolinski@htsoft.pl>
2025-12-24 13:46:50 +0100

    using dynamic linq

--- Source/DocuEye.Linter.Poc/Program.cs
+++ Source/DocuEye.Linter.Poc/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DocuEye.Structurizr.DSL.Model;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+
 Console.WriteLine("Hello, World!");
+
+
+var workspace = new StructurizrWorkspace();
+
+workspace.Model = new StructurizrModel();
+
+workspace.Model.AddModelElement(new StructurizrSoftwareSystem("testsystem")
+{
+    Name = "testsystem",
+    Description = "A test system",
+    Tags = new List<string> { "Tag1", "Tag2" }
+});
+workspace.Model.AddModelElement(new StructurizrContainer("testperson", "testsystem")
+{
+    Description = "A test container",
+    Tags = new List<string> { "Tag1", "Tag3" }
+});
+
+var result = workspace.Model.Elements.AsQueryable().Where("Name == \"testsystem\" AND Tags.Contains(\"Tag1\")").Count();
+
+Console.WriteLine(result);
