120873eb6939e261b4c780d4b72767a80637d6eb
Jacek Zwoliński <jacek.zwolinski@htsoft.pl>
2025-12-24 14:15:51 +0100

    working on linter poc

--- Source/DocuEye.Linter.Poc/Program.cs
+++ Source/DocuEye.Linter.Poc/Program.cs
@@ -1,27 +1,48 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using DocuEye.Structurizr.DSL.Model;
-using System.Linq;
+using DocuEye.Linter.Model;
 using System.Linq.Dynamic.Core;
 
 Console.WriteLine("Hello, World!");
 
-
-var workspace = new StructurizrWorkspace();
-
-workspace.Model = new StructurizrModel();
-
-workspace.Model.AddModelElement(new StructurizrSoftwareSystem("testsystem")
+var elements = new List<LinterModelElement>
 {
-    Name = "testsystem",
-    Description = "A test system",
-    Tags = new List<string> { "Tag1", "Tag2" }
-});
-workspace.Model.AddModelElement(new StructurizrContainer("testperson", "testsystem")
+    new LinterModelElement { Name = "testsystem", Tags = new List<string> { "SoftwareSystem", "Tag2" } },
+    new LinterModelElement { Name = "anothersystem", Tags = new List<string> { "Container", "Tag3" } },
+    new LinterModelElement { Name = "testsystem", Tags = new List<string> { "Tag3" } },
+};
+
+Dictionary<string, object> variables = new Dictionary<string, object>
 {
-    Description = "A test container",
-    Tags = new List<string> { "Tag1", "Tag3" }
-});
+    { "AllowedTechnologies", new[] { "REST" } }
+};
 
-var result = workspace.Model.Elements.AsQueryable().Where("Name == \"testsystem\" AND Tags.Contains(\"Tag1\")").Count();
+var rules = new List<LinterRule>
+{
+    new LinterRule
+    {
+        Key =  "ContainerTechnologyMustBeDefined",
+        Name = "Container technology must be defined",
+        Description = "Each container must have a defined technology to ensure clarity in the architecture.",
+        Severity = "Warning",
+        Type = "Element",
+        Expression = "Tags.Contains(\"Container\") and Technology == null",
+        HelpLink = "https://example.com/rules/container-technology-must-be-defined"
+    },
+    new LinterRule
+    {
+        Key = "ContainerAllowedTechnologies",
+        Name = "Container technology must be one of the allowed technologies",
+        Description = "Containers should use only approved technologies to maintain consistency and supportability.",
+        Severity = "Warning",
+        Type = "Element",
+        Expression = "Tags.Contains(\"Container\") and Technology not_in (\"REST\")",
+        HelpLink = "https://example.com/rules/container-allowed-technologies"
+    }
+};
+foreach (var rule in rules)
+{
+    Console.WriteLine($"Rule: {rule.Name}, Expression: {rule.Expression}");
+    var result = elements.AsQueryable().Where(rule.Expression, variables).Count();
 
-Console.WriteLine(result);
+    Console.WriteLine(result);
+}
