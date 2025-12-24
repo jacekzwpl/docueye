b30dbdfca56d56b4e9a745131982021c729ac420
Jacek Zwoliński <jacek.zwolinski@htsoft.pl>
2025-12-24 14:15:51 +0100

    working on linter poc

---
+++ Source/DocuEye.Linter.Model/LinterModelElement.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DocuEye.Linter.Model
+{
+    public class LinterModelElement
+    {
+         public string Name { get; set; }
+         public IEnumerable<string> Tags { get; set; }
+         public string Technology { get; set; }
+         
+    }
+}
