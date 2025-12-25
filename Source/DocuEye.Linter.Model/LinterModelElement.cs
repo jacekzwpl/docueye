08946e3b662c042b7800b2fe31a8421ca60220a2
Jacek Zwoliński <jacek.zwolinski@htsoft.pl>
2025-12-25 10:28:26 +0100

    poc all rules

--- Source/DocuEye.Linter.Model/LinterModelElement.cs
+++ Source/DocuEye.Linter.Model/LinterModelElement.cs
@@ -5,6 +5,8 @@ namespace DocuEye.Linter.Model
 {
     public class LinterModelElement
     {
+         public string Identifier { get; set; }
+         public string? ParentIdentifier { get; set; } = null;
          public string Name { get; set; }
          public IEnumerable<string> Tags { get; set; }
          public string Technology { get; set; }
---
+++ Source/DocuEye.Linter.Model/LinterModelRelationship.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DocuEye.Linter.Model
+{
+    public class LinterModelRelationship
+    {
+        
+        public LinterModelElement Source { get; set; }
+        public LinterModelElement Destination { get; set; }
+        public string? Technology { get; set; } = null;
+    }
+}
