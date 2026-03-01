33926e76fde6c4c826f5ad21c089248f3611c774
Jacek Zwoliński <jacek.zwolinski@htsoft.pl>
2026-03-01 19:06:58 +0100

    convert dynamic view to mermaid

---
+++ Source/DocuEye.Mermaid.Tests/SequenceDiagramCreatorTests.cs
@@ -0,0 +1,39 @@
+﻿using DocuEye.ViewsKeeper.Model;
+
+namespace DocuEye.Mermaid.Tests
+{
+    public class SequenceDiagramCreatorTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CreateFromDynamicViewReturnsExpectedSequenceDiagram()
+        {
+            // Arrange
+            var dynamicView = new DynamicView
+            {
+                Elements = new[]
+                {
+                    new ElementView { Id = "A", Name = "Element A" },
+                    new ElementView { Id = "B", Name = "Element B" }
+                },
+                Relationships = new[]
+                {
+                    new DynamicRelationshipView { SourceId = "A", DestinationId = "B", Description = "calls" },
+                    new DynamicRelationshipView { SourceId = "B", DestinationId = "A", Description = "returns", Response = true }
+                }
+            };
+            var creator = new SequenceDiagramCreator();
+
+            // Act
+            var result = creator.Create(dynamicView);
+
+            // Assert
+            var expected = "sequenceDiagram\r\n\tparticipant A as Element A\r\n\tparticipant B as Element B\r\n\tA ->> B: calls\r\n\tB -->> A: returns";
+            Assert.That(result.Trim(), Is.EqualTo(expected.Trim()));
+        }
+    }
+}
---
+++ Source/DocuEye.Mermaid/SequenceDiagramCreator.cs
@@ -0,0 +1,26 @@
+﻿using DocuEye.ViewsKeeper.Model;
+using System;
+using System.Text;
+
+namespace DocuEye.Mermaid
+{
+    public class SequenceDiagramCreator
+    {
+        public string Create(DynamicView dynamicView) { 
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("sequenceDiagram");
+            foreach (var element in dynamicView.Elements)
+            {
+                //sb.AppendLine("\t");
+                sb.AppendLine($"\tparticipant {element.Id} as {element.Name}");
+            }
+            foreach (var relationship in dynamicView.Relationships)
+            {
+                string relationshipType = relationship.Response.HasValue && relationship.Response.Value ? "-->>" : "->>";
+                //sb.AppendLine("\t");
+                sb.AppendLine($"\t{relationship.SourceId} {relationshipType} {relationship.DestinationId}: {relationship.Description}");
+            }
+            return sb.ToString();
+        }
+    }
+}
