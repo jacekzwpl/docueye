7b143864aaa70091d5c3c41f1433589efba06fb5
Jacek Zwoliński <jacek.zwolinski@htsoft.pl>
2026-03-03 21:30:46 +0100

    clean up

--- Source/DocuEye.Structurizr.Json.Model.Maps/StructurizrJsonContainerViewMap.cs
+++ Source/DocuEye.Structurizr.Json.Model.Maps/StructurizrJsonContainerViewMap.cs
@@ -3,7 +3,6 @@ using DocuEye.WorkspaceImporter.Api.Model.Views;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace DocuEye.Structurizr.Json.Model.Maps
 {
